1696b64b3971642a9c7dee80da276174c9a439e8
Ben Beadle <benjbeadle@gmail.com>
2013-04-25 00:41:42 -0500

    Finished Java app and printed report to pdf.

--- eclipsePartFive/PartFive.java
+++ eclipsePartFive/PartFive.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 
 public class PartFive
 {
-	public static void one(Connection conn) throws SQLException {
+	public static void one(Connection conn, Scanner input) throws SQLException {
 		Statement s = conn.createStatement(); 
 	 	System.out.println("This query will show all of the organizations and their leaders if they have any.");
 
+	 	//Get all of the organizations and their leaders
 		s.executeQuery ("SELECT Faculty.FacultyID, Faculty.Name AS LeaderName, " +
 				"StudentOrganization.StudentOrganizationID, StudentOrganization.Name AS Organization" + 
 				", Category FROM Faculty RIGHT JOIN(StudentOrganization LEFT JOIN Leads ON Leads.StudentOrganizationID" +
 				"= StudentOrganization.StudentOrganizationID) ON Faculty.FacultyID = Leads.FacultyID");
 		ResultSet rs = s.getResultSet();
+		//Print the result out!
 		while (rs.next ())
 		{
-		   int idVal = rs.getInt ("Faculty.FacultyID");
-		   String nameVal = rs.getString ("LeaderName");
 		   System.out.println (
 				   "Organization '" + rs.getString("Organization")
 				   + "' with leader '" + rs.getString("LeaderName") + "'");
@@ -23,11 +23,13 @@ public class PartFive
 		rs.close ();
 		s.close ();
 	}
-	public static void two(Connection conn) throws SQLException {
+	public static void two(Connection conn, Scanner input) throws SQLException {
+		
+		//Get all of the students and their advisor's name!
 		Statement s = conn.createStatement();
-
 		s.executeQuery ("SELECT Student.Name, Faculty.Name AS Advisor FROM Student, Faculty WHERE Student.Advisor=Faculty.FacultyID");
 		ResultSet rs = s.getResultSet();
+		//Print the results out!
 		while (rs.next ())
 		{
 		   System.out.println (
@@ -37,9 +39,8 @@ public class PartFive
 		rs.close ();
 		s.close ();
 	}
-	public static void three(Connection conn) throws SQLException {
+	public static void three(Connection conn, Scanner input) throws SQLException {
 		Statement s = conn.createStatement ();
-		Scanner scanner = new Scanner( System.in );
 		System.out.print("You're going to be removing a student from an organization.\n");
 		System.out.print("Below are the current organizations and their perspective ids.\n");
 
@@ -57,10 +58,7 @@ public class PartFive
 		rs.close ();
 		
 		System.out.print("Type in the ogranization ID that you want to select: ");
-		String organizationID = scanner.nextLine();
-		
-		//For testing purposes to check that it prints what you entered.
-		System.out.print(organizationID);
+		String organizationID = input.nextLine();
 		
 		System.out.print("\nYou selected the organization with this id:" + organizationID + ".\n");
 		System.out.print("Below are the current members for said organization and their perspective UINs\n");
@@ -80,16 +78,12 @@ public class PartFive
 		rs.close ();	
 		
 		System.out.print("\nType in the student UIN that you want to delete from the orgnanization: ");
-		String studentUIN = scanner.nextLine();
-		
-		System.out.print(studentUIN);
-		
-		System.out.print("\nCreating the statement...\n");
+		String studentUIN = input.nextLine();
 
 		String sqlStatment = "DELETE FROM MemberOf WHERE UIN=" + studentUIN + " AND StudentOrganizationID =" + organizationID;
 		s.executeUpdate(sqlStatment);
 
-		System.out.print("\nBelow is the organization you initally selectd and the current members.\n");
+		System.out.print("\nBelow is the organization you initally selected and the current members.\n");
 		//Now, Let's see the results. We're printing out the members of the organization selected.	
 		s.executeQuery ("SELECT Student.Name, Student.UIN FROM Student, MemberOf WHERE MemberOf.StudentOrganizationID = " + organizationID +" AND Student.UIN = MemberOf.UIN");
 		rs = s.getResultSet ();
@@ -104,9 +98,8 @@ public class PartFive
 		rs.close ();	
 		
 		s.close ();
-		scanner.close();
 	}
-	public static void four(Connection conn) throws SQLException {
+	public static void four(Connection conn, Scanner input) throws SQLException {
 		String percentage = "0";
 		String facultyid = "-1";
 		String upperfacultyid = "-1";
@@ -115,7 +108,6 @@ public class PartFive
        try
        {
 			//Get desired action and parameters
-    	   Scanner input = new Scanner(System.in);
 
 			System.out.println("Options:\n	1:  Alter salary of a specific faculty id\n	2:  Alter salary of a specific range of faculty ids\n	3:  Give a sweeping raise/cut to all faculty");
 			option = input.nextInt();
@@ -159,8 +151,6 @@ public class PartFive
 				System.out.println("No faculty found with that id");
 			else
 				System.out.println(modCount + " row(s) modified");
-
-			input.close();
        }
        catch (SQLException sql){
     	   System.err.println(sql.getSQLState());
@@ -171,7 +161,7 @@ public class PartFive
            System.err.println ("Some random error occured");
        }
 	}
-	public static void five(Connection conn) throws SQLException {
+	public static void five(Connection conn, Scanner input) throws SQLException {
 		int UIN;
 		String student;
 		String major;
@@ -179,13 +169,12 @@ public class PartFive
 		
 		try{
 			//Get the student's information from the user
-			Scanner input = new Scanner(System.in);
 			System.out.println("Enter the name of the student you wish to add");
 			student = input.nextLine();
 			System.out.println("What is the student's major?");
 			major = input.next();
 						
-			//Assigne the student's UIN
+			//Assign the student's UIN
 			PreparedStatement s;
 			s = conn.prepareStatement("SELECT MAX(UIN) AS UIN FROM Student");
 			s.execute();
@@ -263,7 +252,7 @@ public class PartFive
 	   while(keepGoing) {
 		   System.out.println("1. List all organizations and their corresponding leaders.");
 		   System.out.println("2. List all student's and their Advisor's name.");
-		   System.out.println("3. Remove a student from a class.");
+		   System.out.println("3. Remove a student from an organization.");
 		   System.out.println("4. Alter all faculty's salary by a certain percentage.");
 		   System.out.println("5. Add a student to the database.");
 		   System.out.println("6. Quit application.");
@@ -275,11 +264,11 @@ public class PartFive
 		   String in = inputer.nextLine();
 		   try {
 			   switch(in) {
-				   case "1": one(conn);				break;
-				   case "2": two(conn); 			break;
-				   case "3": three(conn); 			break;
-				   case "4": four(conn); 			break;
-				   case "5": five(conn); 			break;
+				   case "1": one(conn, inputer);	break;
+				   case "2": two(conn, inputer); 	break;
+				   case "3": three(conn, inputer); 	break;
+				   case "4": four(conn, inputer); 	break;
+				   case "5": five(conn, inputer); 	break;
 				   case "6": keepGoing = false; 	break;
 			   }
 			} catch (SQLException e) {
